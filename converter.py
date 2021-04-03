f637f3c5630250fe1614eca5629f78ef29057926
Brian Do <briancldo@gmail.com>
2021-04-03 19:30:59 -0400

    implements converter for sdk format

--- converter.py
+++ converter.py
@@ -1,2 +1,21 @@
-def convert(file, type):
-    grid = []
+from grid import Grid
+
+def convert_sdk(filestream):
+    content: str = filestream.read()
+    lines = content.replace('.', '0').split('\n')
+    grid = [list(line) for line in lines]
+    return Grid(grid)
+
+converters = {
+    'sdk': convert_sdk
+}
+
+def convert(file: str) -> Grid:
+    filestream = open(file)
+    type = file.split('.').pop()
+    
+    try:
+       converter = converters[type]
+       return converter(filestream)
+    except KeyError:
+        raise Exception(f'Invalid file type: {type}')
